28d73f5b5480a148229f954b989f3ee7d4c5f7cb
PrityDas <prityd825@gmail.com>
2023-09-27 18:28:47 +0600

    initial push

---
+++ Exploring_DOM/js/script.js
@@ -0,0 +1,42 @@
+let val;
+val=this;
+val = window;
+val = window.document;
+val = document;
+val = document.all;
+val = document.all[0];
+val = document.all[2];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+
+val = document.forms;
+val = document.form[0];
+val = document.forms[0].method;
+val = document.forms[0].action;
+
+
+
+val = document.links;
+val = document.links[0];
+val = document.images;
+
+val = document.scripts;
+val = document.scripts[0].src;
+val = document.scripts[0].getAttribute('src');
+
+links = document.links;
+let linkArr = Array.from(links);
+
+linkArr.forEach(function(link){
+    console.log(link.getAttribute('href'));
+});
+
+console.log(links);
+
+console.log(val);
