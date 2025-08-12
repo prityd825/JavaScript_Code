a16e9321b30f09177ceb89d90180e7488d711d5c
prityd825 <prity.rani@bracits.com>
2025-08-12 21:37:33 +0600

    fix: add new folder

--- Change-Html/script.js
+++ Change-Html/script.js
@@ -2,5 +2,5 @@ function submit() {
     // document.getElementById('demo').innerHTML = 'Change the html';
     document.getElementById('small-text').style = 'color: blue';
     document.getElementById('name').style.display = 'none';
-    // document.getElementById('name').style.display = 'block';
+    document.getElementById('name').style.display = 'block';
 }
