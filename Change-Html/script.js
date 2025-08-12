d0b48bce14231e6adada0b949948f6a69edc46e3
prity.rani@bracits.com <prity.rani@bracits.com>
2025-08-12 21:35:21 +0600

    fix: add new folder

---
+++ Change-Html/script.js
@@ -0,0 +1,6 @@
+function submit() {
+    // document.getElementById('demo').innerHTML = 'Change the html';
+    document.getElementById('small-text').style = 'color: blue';
+    document.getElementById('name').style.display = 'none';
+    // document.getElementById('name').style.display = 'block';
+}
